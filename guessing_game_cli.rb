74a37b9b665e05458167d6380cfc68a21fba4c8e
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 15:31:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,12 +3,12 @@ def run_guessing_game
  roll = rand(1..6)
  user_input = gets.chomp
  if user_input == roll
-   puts"You guessed the correct number!"
+   return "You guessed the correct number!"
  elsif user_input != roll
-  puts "Sorry! The computer guessed #{roll}."
+  return "Sorry! The computer guessed #{roll}."
   elsif user_input == "exit"
-  puts "Goodbye!"
+  return "Goodbye!"
   else
-  puts "Invalid input"
+  return "Invalid input"
 	end
 end
