d7d3fce035992d682f9ebd050e77f7d70b3afaf9
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 15:27:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,8 @@ def run_guessing_game
  roll = rand(1..6)
  user_input = gets.chomp
  if user_input == roll
-   puts
+   puts"You guessed the correct number!"
+ else
 if user_input == "exit"
   puts "Goodbye!"
 else
