15ffa08ff90e6cc413dd0363809ae33fee506e46
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 15:18:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
  	puts "Guess a number betwen 1 and 6."
 	roll = 1+rand(6)
-	user_input = gets.chomp.to_s
+	user_input = gets.chomp
     if user_input == roll 
     	return "You guessed the correct number!"
     elsif user_input != roll
