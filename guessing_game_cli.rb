6e8a2fac0d83fce37400d4000a550fde8e32d462
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 15:14:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,5 +8,10 @@ def run_guessing_game
     else
       puts "The computer guessed #{number}."
     end
-    
+     puts "Guess a number between 1 and 6.\n"
+    number = rand(1..6).to_s
+    input = gets.chomp
+  end
+
+  puts "Goodbye!"
 end
